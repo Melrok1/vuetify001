8a7a02158f18571b646e9be2243cc135501dc506
Melrok <martin.blascak86@gmail.com>
2019-10-09 18:45:58 +0200

    Pridane postranne menu

--- vuetify001/src/App.vue
+++ vuetify001/src/App.vue
@@ -2,20 +2,30 @@
   <html>
     <app-header></app-header>
     <main-content></main-content>
+    <prog-menu v-bind:progMenu="progMenu"></prog-menu>
   </html>
 </template>
 
 <script>
 import Header from './components/header.vue';
 import MainContent from './components/mainContent.vue';
+import ProgMenuButton from './components/progMenu.vue';
 
 export default {
   components: {
     'app-header': Header,
-    'main-content': MainContent
+    'main-content': MainContent,
+    'prog-menu': ProgMenuButton
   },
   data () {
     return {
+      progMenu: [
+        {adresa: 'https://www.pngfly.com/png-r1hbmm/download.html', menoPolozky: 'HTML', ikona: ''},
+        {adresa: 'https://mycolor.space/gradient?ori=to+top&hex=%23051937&hex2=%23A8EB12&sub=1', menoPolozky: 'CSS', ikona: ''},
+        {adresa: '#', menoPolozky: 'JavaScript', ikona: ''},
+        {adresa: '#', menoPolozky: 'Vue.js', ikona: ''},
+        {adresa: '#', menoPolozky: 'Node.js', ikona: ''}
+      ]
     }
   }
 }
---
+++ vuetify001/src/components/progMenu.vue
@@ -0,0 +1,63 @@
+<template>
+    <div id="skill">
+        <ul>
+            <li v-for="(link, index) in progMenu" v-bind:key="index">  
+                <a v-bind:href="link.adresa"> {{ link.menoPolozky }} </a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+
+
+
+<script>
+export default {
+  props: ['progMenu'],
+  data () {
+    return {}
+  }
+}
+</script>
+
+
+
+
+
+<style scoped>
+#skill {
+    background: #cecccc;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    width: 15%;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+h1 {
+    margin-right: 1rem ;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: block;
+  margin: 0 10px;
+  margin-top: 0.4rem;
+}
+
+a {
+  margin: 0.15rem;
+  cursor: pointer;
+  color: #54b983;
+  text-decoration: none;
+  font-weight: 800;
+}
+</style>
